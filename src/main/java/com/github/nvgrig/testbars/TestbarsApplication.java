376f2010610a8fadfcadbe26d11bf492262ff204
Nikita Grigoryev <nvgrigoryev@gmail.com>
2021-09-11 18:25:35 +0400

    Initial commit

---
+++ src/main/java/com/github/nvgrig/testbars/TestbarsApplication.java
@@ -0,0 +1,13 @@
+package com.github.nvgrig.testbars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestbarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/nvgrig/testbars/TestbarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nvgrig.testbars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
